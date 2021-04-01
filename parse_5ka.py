f2e02c21ead79b2323e1ae99ddcbaced2276b778
Jane <jane19937788@gmail.com>
2021-04-01 18:36:12 +0300

    home work lesson 1

--- parse_5ka.py
+++ parse_5ka.py
@@ -3,6 +3,7 @@ import json
 import requests
 import time
 
+
 class Parser5ka:
     headers = {
         "User-Agent": "Mask"
@@ -24,9 +25,9 @@ class Parser5ka:
             product_path = self.save_path.joinpath(f"{product['id']}.json")
             self._save(product, product_path)
 
-    def _parse(self, url):
+    def _parse(self, url, *args, **kwargs):
         while url:
-            response = self._get_responce(url)
+            response = self._get_responce(url, *args, **kwargs)
             data: dict = response.json()
             url = data.get('next')
             for product in data.get('results', []):
@@ -34,6 +35,36 @@ class Parser5ka:
 
     def _save(self, data, file_path):
         file_path.write_text(json.dumps(data, ensure_ascii=False))
+
+
+class Parser5kaGroupBy(Parser5ka):
+
+    def __init__(self, start_url: str, save_path: Path, url_for_group: str = 'https://5ka.ru/api/v2/categories/'):
+        super().__init__(start_url, save_path)
+        self.url_for_group = url_for_group
+
+    def _get_group_name(self) -> list:
+        request = Parser5ka._get_responce(self, self.url_for_group)
+        for categories in request.json():
+            yield categories
+
+    def _get_params_names(self, categories: dict) -> dict:
+        return {
+            "categories": int(categories["parent_group_code"])
+        }
+
+    def run(self):
+        for category in self._get_group_name():
+            category_data = {
+                "name": category["parent_group_name"],
+                "code": int(category["parent_group_code"]),
+                "products": [product for product in Parser5ka._parse(self, self.start_url, params=self._get_params_name(category))]
+            }
+
+            Parser5ka._save(self,
+                            data=category_data,
+                            file_path=self.save_path.joinpath(f"{category_data['name']}.json"))
+
 
 
 def get_save_path(dir_name):
@@ -44,5 +75,7 @@ def get_save_path(dir_name):
 
 
 if __name__ == '__main__':
-    parser = Parser5ka("https://5ka.ru/api/v2/special_offers/", get_save_path('products'))
+    # parser = Parser5ka("https://5ka.ru/api/v2/special_offers/", get_save_path('products'))
+    parser = Parser5kaGroupBy("https://5ka.ru/api/v2/special_offers/", get_save_path('products'))
     parser.run()
+
