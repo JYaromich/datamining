415541faa76e28c21296ef6e46775900b066a434
Jane <jane19937788@gmail.com>
2021-04-18 12:37:07 +0300

    end of home work 5

--- gb_parse/pipelines.py
+++ gb_parse/pipelines.py
@@ -7,6 +7,8 @@
 # useful for handling different item types with a single interface
 
 import pymongo
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy.http.request import Request
 
 
 class GbParsePipeline:
@@ -15,4 +17,15 @@ class GbParsePipeline:
 
     def process_item(self, item, spider):
         self.db[spider.name].insert_one(item)
+        return item
+
+
+class GbImagesDowloadPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        for url in item.get('photos', []):
+            yield Request(url)
+
+    def item_completed(self, results, item, info):
+        if 'photos' in item:
+            item['photos'] = [itm[1] for itm in results]
         return item
